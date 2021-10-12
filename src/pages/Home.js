fc25ca11d45d89941f5fbc4b720fa96ba1585b99
AndreCosta101 <andre.costa101@gmail.com>
2021-10-12 16:10:21 -0300

    Utilizando StyleSheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,65 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 export function Home() {
   return (
     <>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>React Native man</Text>
-        <Text style={{ alignSelf: 'center', marginBottom: 20 }}>ignite</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome, Andre</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="New Skill"
+          placeholderTextColor='#555'
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+        >
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+
+        <Text style={[styles.title, { marginTop: 20 }]}>
+          My Skills
+        </Text>
       </View>
+
+
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: 15,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#A370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+  }
+})
